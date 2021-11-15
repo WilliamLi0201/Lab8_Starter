9e895d0ab7e58ea813926de61b4999ba24d442db
William Li <chl057@ucsd.edu>
2021-11-14 23:52:58 -0800

    All updates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Lab 8 - Starter
+1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+Within a Github action that runs whenever code is pushed. Github action gives us the convinience to trigger specific event automatically. Whenever code is pushed, tests will be automatically executed.
+
+2) Would you use an end to end test to check if a function is returning the correct output?
+No.
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+No. Because this feature will be interacted with other features on application level, but unit test cannot do that for us. It there is a problem, we cannot promise that the problem is because of "message" feature.
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+Yes. Because "max message length" feature exists individually, even all other features change, this feature will not be affected as long as the rule does not change (80 as limit).
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,85 @@
 // unit.test.js
 
+const { test, expect } = require('@jest/globals');
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('function isPhoneNumber test 1', () => {
+    expect(functions.isPhoneNumber('858-247-9352')).toBe(true);
+});
+
+test('function isPhoneNumber test 2', () => {
+    expect(functions.isPhoneNumber('888-888-8888')).toBe(true);
+});
+
+test('function isPhoneNumber test 3', () => {
+    expect(functions.isPhoneNumber('William')).toBe(false);
+});
+
+test('function isPhoneNumber test 4', () => {
+    expect(functions.isPhoneNumber('chl057@ucsd.edu')).toBe(false);
+});
+
+test('function isEmail test 1', () => {
+    expect(functions.isEmail('chl057@ucsd.edu')).toBe(true);
+});
+
+test('function isEmail test 2', () => {
+    expect(functions.isEmail('lch@gmail.com')).toBe(true);
+});
+
+test('function isEmail test 3', () => {
+    expect(functions.isEmail('William')).toBe(false);
+});
+
+test('function isEmail test 4', () => {
+    expect(functions.isEmail('858-247-9352')).toBe(false);
+});
+
+test('function isStrongPassword test 1', () => {
+    expect(functions.isStrongPassword('Kuanye')).toBe(true);
+});
+
+test('function isStrongPassword test 2', () => {
+    expect(functions.isStrongPassword('ChenghaoLi')).toBe(true);
+});
+
+test('function isStrongPassword test 3', () => {
+    expect(functions.isStrongPassword('ABC')).toBe(false);
+});
+
+test('function isStrongPassword test 4', () => {
+    expect(functions.isStrongPassword('1Kuanye')).toBe(false);
+});
+
+test('function isDate test 1', () => {
+    expect(functions.isDate('02/01/2001')).toBe(true);
+});
+
+test('function isDate test 2', () => {
+    expect(functions.isDate('2/1/2019')).toBe(true);
+});
+
+test('function isDate test 3', () => {
+    expect(functions.isDate('0/0/0')).toBe(false);
+});
+
+test('function isDate test 4', () => {
+    expect(functions.isDate('Kuanye')).toBe(false);
+});
+
+test('function isHexColor test 1', () => {
+    expect(functions.isHexColor('000000')).toBe(true);
+});
+
+test('function isHexColor test 2', () => {
+    expect(functions.isHexColor('FFFFFF')).toBe(true);
+});
+
+test('function isHexColor test 3', () => {
+    expect(functions.isHexColor('QWERTY')).toBe(false);
+});
+
+test('function isHexColor test 4', () => {
+    expect(functions.isHexColor('0')).toBe(false);
+});
